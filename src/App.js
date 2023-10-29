46463f0d0b0e0c923de587af264990b5536ff76d
hector1234567 <hectorbg303@gmail.com>
2023-10-29 12:24:00 +0100

    Children composition

--- src/App.js
+++ src/App.js
@@ -54,18 +54,25 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies}/>
-      <Main movies={movies}/>
+      <NavBar>
+        <Search/>
+        <NumResults movies={movies}/>
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MoviesList movies={movies}/>
+        </ListBox>
+        <WatchedBox/>
+      </Main>
     </>
   );
 }
 
-function NavBar({movies}) {
+function NavBar({children}) {
   return (
     <nav className="nav-bar">
       <Logo/>
-      <Search/>
-      <NumResults movies={movies}/>
+      {children}
     </nav>
   );
 }
@@ -100,16 +107,15 @@ function NumResults({movies}) {
   );
 }
 
-function Main({movies}) {
+function Main({children}) {
   return (
     <main className="main">
-      <ListBox movies={movies}/>
-      <WatchedBox/>
+      {children}
     </main>
   );
 }
 
-function ListBox({movies}) {
+function ListBox({children}) {
   const [isOpen1, setIsOpen1] = useState(true);
   return (
     <div className="box">
@@ -119,7 +125,7 @@ function ListBox({movies}) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MoviesList movies={movies}/>}
+      {isOpen1 && children}
     </div>
   );
 }
