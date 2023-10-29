3f5deaf1252c48f1a99510ba14959902a4a4ed0e
hector1234567 <hectorbg303@gmail.com>
2023-10-29 16:15:25 +0100

    Rating Stars

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import RatingStars from './RatingStars';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RatingStars numStars={10}/>
+    {/* <App /> */}
   </React.StrictMode>
 );
